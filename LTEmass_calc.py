7d86fc1c86325e8adb705540765d2d757b0c754f
gracekrahm <106852743+gracekrahm@users.noreply.github.com>
2022-10-25 14:11:55 -0400

    Update LTEmass_calc.py

--- LTEmass_calc.py
+++ LTEmass_calc.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
                 ncl_list.append(ncl)
                 t12 = np.where(ncl==clumps12, COtemp12, np.nan)
                 t13 = np.where(ncl==clumps12, COtemp12, np.nan)
-                print('t13 max:', np.nanmax(t13), 't12 max:',np.nanmax(t12)
+                print('t13 max:', np.nanmax(t13), 't12 max:',np.nanmax(t12))
                 mom013 = np.nansum(t13, axis=0)
                 mom012 = np.nansum(t12,axis=0)
                 if (26*mom013 <0.25*mom012):
